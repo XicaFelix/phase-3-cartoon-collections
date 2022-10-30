91b79c3bbbeedb27cf7b15a06682ca22aa0c0548
Xica Felix <xicasfelix@gmail.com>
2022-10-30 19:24:10 -0400

    commit tree dc0fab4b320a314422287e730dcb7407036e3090

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,42 @@
-def roll_call_dwarves# code an argument here
+def roll_call_dwarves array
   # Your code here
+  resp = []
+  array.map.with_index(1) do |item, index|
+   resp << "#{index}. #{item}"
+  end
+  puts resp
 end
 
-def summon_captain_planet# code an argument here
+# roll_call_dwarves (%w[Dopey Grumpy Bashful])
+def summon_captain_planet array
   # Your code here
+  resp = []
+  array.map do |item|
+    item = item.capitalize() + "!"
+    resp << item
+  end
+  resp
+end
+# puts summon_captain_planet (%w[earth wind fire water heart])
+
+def long_planeteer_calls array
+  # Your code here
+  array.any? {|item| item.length > 4}
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
-end
 
-def find_the_cheese# code an argument here
+def find_the_cheese array
   # the array below is here to help
+  resp = []
   cheese_types = ["cheddar", "gouda", "camembert"]
+  cheese_types.map do |cheese|
+     resp = array.include?(cheese)
+     if resp == true
+      return cheese
+     else
+      return nil
+     end
+  end
 end
+
+puts find_the_cheese(%w[ham cellphone computer])
